0523fda4c210ca4c664b2d32d89b25e554fed0cd
@tomas.acuna <tomas.acuna@unraf.edu.ar>
2023-04-25 14:33:05 -0300

    Movement Test

--- Assets/Scripts/Dummys/Player.cs
+++ Assets/Scripts/Dummys/Player.cs
@@ -6,21 +6,60 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     float HP = 193f;
-    
-    // Start is called before the first frame update
+    float vertspid = -4f;
+    bool canIjump = true;
+    private Rigidbody2D body;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        _plaseJump();
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * 5f, vertspid);
+        vertspid += -0.03f;
+        vertspid = Clamp(vertspid, -4f, 8f);
+    }
+
+    private void _plaseJump()
+    {
+        Debug.Log(Input.GetAxis("Jump"));
+        if (canIjump && Input.GetButton("Jump"))
+        {
+            vertspid +=  8f;
+            canIjump = false;
+        }
+
+    }
+
+
+    private float Clamp(float x, float y, float z)
+    {
+        if (x.CompareTo(y) < 0) return y;
+        else if (x.CompareTo(z) > 0) return z;
+        else return x;
     }
 
     public string GetHP()
     {
         return HP.ToString();
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.contacts[0]);
+        if (collision.contacts[0].point.y < transform.position.y)
+        {
+            canIjump = true;
+        }
+        else
+        {
+            canIjump = false;
+        }
+    }
+    
+
 }
