1289274968834a494fb6528218ac509d4eab7553
Sergio Damián Bressán <sergiodb95@gmail.com>
2023-05-10 19:02:59 -0300

    fixed_colisions

--- Assets/Scripts/Enemy/EnemyCollision.cs
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -7,21 +7,23 @@ public class EnemyCollision : MonoBehaviour
     private Collider2D otherCollider;
     private Bounds bounds;
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        otherCollider = collision.collider;
-        bounds = GetComponent<BoxCollider2D>().bounds;
-        
-        if (otherCollider.OverlapPoint(new Vector2(bounds.min.x, bounds.center.y)))
+    void OnTriggerStay2D(Collider2D col){
+        if (col.CompareTag("Borde"))
         {
-            gameObject.GetComponent<EnemyMovement>()._leftMovement = false;
-            transform.localScale = new Vector3(3.5f, 3.5f, 0);
-        }
+            otherCollider = col.GetComponent<Collider2D>();
+            bounds = GetComponent<BoxCollider2D>().bounds;
+            
+            if (otherCollider.OverlapPoint(new Vector2(bounds.min.x, bounds.center.y)))
+            {
+                gameObject.GetComponent<EnemyMovement>()._leftMovement = false;
+                transform.localScale = new Vector3(3.5f, 3.5f, 0);
+            }
 
-        if (otherCollider.OverlapPoint(new Vector2(bounds.max.x, bounds.center.y)))
-        {
-            gameObject.GetComponent<EnemyMovement>()._leftMovement = true;
-            transform.localScale = new Vector3(-3.5f, 3.5f, 0);
+            if (otherCollider.OverlapPoint(new Vector2(bounds.max.x, bounds.center.y)))
+            {
+                gameObject.GetComponent<EnemyMovement>()._leftMovement = true;
+                transform.localScale = new Vector3(-3.5f, 3.5f, 0);
+            }
         }
     }
 }
