b7746984ddcf25e5d25ff3dc62cdd88867ac0dda
alva531 <alvaro.jorge531@gmail.com>
2023-06-09 20:21:45 -0300

    resize 1:1

--- Assets/Assets/Tutorial tiled/Background Objets/obj_lvl1_elevator_01.tsx
+++ Assets/Assets/Tutorial tiled/Background Objets/obj_lvl1_elevator_01.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="obj_lvl1_elevator_01" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="obj_lvl1_elevator_01.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,7.99609 0.0078125,8.00781"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
--- Assets/Assets/Tutorial tiled/tile_black.tsx
+++ Assets/Assets/Tutorial tiled/tile_black.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="tile_black" tilewidth="32" tileheight="32" tilecount="1" columns="1">
  <image source="tile_black.png" width="32" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
