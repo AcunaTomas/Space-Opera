27328fb4fd1e02a80e237d29274bc1552116731c
alva531 <alvaro.jorge531@gmail.com>
2023-06-16 16:49:29 -0300

    first

--- Assets/Assets/Tutorial tiled/tile_lvl1_outside.tsx
+++ Assets/Assets/Tutorial tiled/tile_lvl1_outside.tsx
@@ -1,32 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="tile_lvl1_outside" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="tile_lvl1_outside.png" width="64" height="64"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="2">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="3">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
--- Assets/Assets/Tutorial tiled/tile_lvl1_plataform_02.tsx
+++ Assets/Assets/Tutorial tiled/tile_lvl1_plataform_02.tsx
@@ -1,32 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="tile_lvl1_plataform_02" tilewidth="32" tileheight="32" tilecount="9" columns="3">
  <image source="tile_lvl1_plataform_02.png" width="96" height="96"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32.0111,14.9778 3,15 2.99609,14.0039 2,13.9961 2.00391,12.9922 1.00391,12.9922 1.00391,11.9922 0.0078125,11.9922"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,14.9961 0,14.9961"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="2">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,12.0078 31,12.0078 31.0078,12.9922 30,12.9922 30.0078,13.9766 29,13.9844 29,14.9453 0,15.0039"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="3">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,11.9944 31,12 30.9944,13 30.0056,13 30.0111,14 29.0111,13.9944 29.0111,15 2.98889,15.0056 3.00556,14 2,14.0056 2,12.9944 0.988889,12.9944 0.988889,11.9889 -0.00555556,11.9944"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
