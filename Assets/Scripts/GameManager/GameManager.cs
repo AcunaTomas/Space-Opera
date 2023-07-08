ed10dc93420512087832d286ed432e63c9b6ec63
Sergio Damián Bressán <sergiodb95@gmail.com>
2023-07-08 17:43:35 -0300

    bito_le_gusta_pegarse_solo

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class GameManager : MonoBehaviour
     //estas últimas cosas se borran después, solo es para probar
     public KeyCode MORIR;
     private bool _ePressed = false;
+    public Slider SLIDER;
 
     private void Awake()
     {
@@ -29,7 +31,15 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(MORIR) && !_ePressed)
         {
             _ePressed = true;
-            PLAYER.transform.localPosition = new Vector3 (CHECKPOINT.x, CHECKPOINT.y, CHECKPOINT.z);
+            if (SLIDER.value != 0)
+            {
+                SLIDER.value-=20;
+            }
+            if (SLIDER.value == 0)
+            {
+                PLAYER.transform.localPosition = new Vector3 (CHECKPOINT.x, CHECKPOINT.y, CHECKPOINT.z);
+                SLIDER.value = 60;
+            }
         }
 
         if (Input.GetKeyUp(MORIR))
