25a55d52c728679ba2aab5e64336d5137723b993
alva531 <alvaro.jorge531@gmail.com>
2023-07-08 18:38:35 -0300

    background

---
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_elevator_02.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_elevator_02" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="bg_lvl1_elevator_02.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="5">
+   <object id="5" x="0" y="0">
+    <polygon points="0,0 32,0 32,3.99609 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32.0039,3.99609 0,3.99609"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
