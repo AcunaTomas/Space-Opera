ab857191dfd752123ffbc434122d06586251c221
alva531 <alvaro.jorge531@gmail.com>
2023-07-18 15:42:00 -0300

    enemy follow and flip

--- Assets/Scripts/Enemy/EnemyCollision.cs
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -10,20 +10,20 @@ public class EnemyCollision : MonoBehaviour
     void OnTriggerStay2D(Collider2D col){
         if (col.CompareTag("Borde"))
         {
-            otherCollider = col.GetComponent<Collider2D>();
-            bounds = GetComponent<BoxCollider2D>().bounds;
+            // otherCollider = col.GetComponent<Collider2D>();
+            // bounds = GetComponent<BoxCollider2D>().bounds;
             
-            if (otherCollider.OverlapPoint(new Vector2(bounds.min.x, bounds.center.y)))
-            {
-                gameObject.GetComponent<EnemyMovement>()._leftMovement = false;
-                transform.localScale = new Vector3(1f, 1f, 0);
-            }
+            // if (otherCollider.OverlapPoint(new Vector2(bounds.min.x, bounds.center.y)))
+            // {
+            //     gameObject.GetComponent<EnemyMovement>()._leftMovement = false;
+            //     transform.localScale = new Vector3(1f, 1f, 0);
+            // }
 
-            if (otherCollider.OverlapPoint(new Vector2(bounds.max.x, bounds.center.y)))
-            {
-                gameObject.GetComponent<EnemyMovement>()._leftMovement = true;
-                transform.localScale = new Vector3(-1f, 1f, 0);
-            }
+            // if (otherCollider.OverlapPoint(new Vector2(bounds.max.x, bounds.center.y)))
+            // {
+            //     gameObject.GetComponent<EnemyMovement>()._leftMovement = true;
+            //     transform.localScale = new Vector3(-1f, 1f, 0);
+            // }
         }
     }
 }
