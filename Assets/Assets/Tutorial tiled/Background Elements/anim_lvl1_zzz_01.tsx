07906d19b4bdeadcbbba8afced606dc9444104ea
alva531 <alvaro.jorge531@gmail.com>
2023-07-19 18:00:06 -0300

    npcs and animations

---
+++ Assets/Assets/Tutorial tiled/Background Elements/anim_lvl1_zzz_01.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="anim_lvl1_zzz_01" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="anim_lvl1_zzz_01.png" width="96" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
--- Assets/Assets/Tutorial tiled/Background Elements/bg_lvl1_door_01.tsx
+++ Assets/Assets/Tutorial tiled/Background Elements/bg_lvl1_door_01.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_door_01" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="bg_lvl1_door_01.png" width="32" height="32"/>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_door_01" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="bg_lvl1_door_01.png" width="288" height="128"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="19" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="25" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="250"/>
+   <frame tileid="13" duration="250"/>
+   <frame tileid="16" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="34" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="250"/>
+   <frame tileid="14" duration="250"/>
+   <frame tileid="17" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="32" duration="250"/>
+   <frame tileid="35" duration="250"/>
+  </animation>
+ </tile>
 </tileset>
