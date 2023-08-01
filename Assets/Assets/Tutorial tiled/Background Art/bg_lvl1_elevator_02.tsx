31a83c7efe7757ceccf12c78a124bb5367801bb4
alva531 <alvaro.jorge531@gmail.com>
2023-08-01 17:52:57 -0300

    posible solucion collider (not active)

--- Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_elevator_02.tsx
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_elevator_02.tsx
@@ -2,17 +2,13 @@
 <tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_elevator_02" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="bg_lvl1_elevator_02.png" width="64" height="64"/>
  <tile id="0">
-  <objectgroup draworder="index" id="5">
-   <object id="5" x="0.521739" y="0">
-    <polygon points="0.186111,0.291667 0.378261,0.113889 31.3064,0.109983 31.3879,0.207639 31.4701,0.308611 31.4783,3.99609 0,4 -0.00755303,0.477771"/>
-   </object>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5625" y="0" width="31.4375" height="3.875"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0.0539931,0.165365 0.0937515,0.11457 31.207,0.109407 31.3009,0.222656 31.3976,0.312476 31.3789,3.99609 0,3.99609 0.0107728,0.203133"/>
-   </object>
+   <object id="2" x="0" y="0" width="31.25" height="4.0625"/>
   </objectgroup>
  </tile>
 </tileset>
