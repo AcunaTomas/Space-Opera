807ee38420015171c12c60d840109f8d4ccc2176
alva531 <alvaro.jorge531@gmail.com>
2023-08-29 17:17:20 -0300

    parallax

---
+++ Assets/Assets/Level 3/Background/plx_background_lvl3_tree_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="plx_background_lvl3_tree_01" tilewidth="32" tileheight="32" tilecount="3600" columns="80">
+ <image source="plx_background_lvl3_tree_01.png" width="2560" height="1440"/>
+</tileset>
---
+++ Assets/Assets/Level 3/Background/prx_background_lvl3_tree_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="prx_background_lvl3_tree_02" tilewidth="32" tileheight="32" tilecount="7200" columns="160">
+ <image source="prx_background_lvl3_tree_02.png" width="5120" height="1440"/>
+</tileset>
