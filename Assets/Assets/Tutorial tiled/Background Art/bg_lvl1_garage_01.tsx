2a3ac194af729b811f7dcef18c576fe5f13a6cb3
alva531 <alvaro.jorge531@gmail.com>
2023-10-16 22:24:05 -0300

    mandarina 9

---
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_garage_01.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_garage_01" tilewidth="32" tileheight="32" tilecount="144" columns="24">
+ <image source="bg_lvl1_garage_01.png" width="768" height="192"/>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="700"/>
+   <frame tileid="30" duration="700"/>
+   <frame tileid="36" duration="700"/>
+   <frame tileid="42" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="73">
+  <animation>
+   <frame tileid="73" duration="400"/>
+   <frame tileid="79" duration="400"/>
+   <frame tileid="85" duration="400"/>
+   <frame tileid="91" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="74">
+  <animation>
+   <frame tileid="74" duration="400"/>
+   <frame tileid="80" duration="400"/>
+   <frame tileid="86" duration="400"/>
+   <frame tileid="92" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="75">
+  <animation>
+   <frame tileid="75" duration="400"/>
+   <frame tileid="81" duration="400"/>
+   <frame tileid="87" duration="400"/>
+   <frame tileid="93" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="97">
+  <animation>
+   <frame tileid="97" duration="400"/>
+   <frame tileid="103" duration="400"/>
+   <frame tileid="109" duration="400"/>
+   <frame tileid="115" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="98">
+  <animation>
+   <frame tileid="98" duration="400"/>
+   <frame tileid="104" duration="400"/>
+   <frame tileid="110" duration="400"/>
+   <frame tileid="116" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="99">
+  <animation>
+   <frame tileid="99" duration="400"/>
+   <frame tileid="105" duration="400"/>
+   <frame tileid="111" duration="400"/>
+   <frame tileid="117" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
