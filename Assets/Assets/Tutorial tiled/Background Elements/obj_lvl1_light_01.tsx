cd5caaa910b08cb01b2fa19910522ca812b8d64b
alva531 <alvaro.jorge531@gmail.com>
2023-10-16 23:10:45 -0300

    adding anims

---
+++ Assets/Assets/Tutorial tiled/Background Elements/obj_lvl1_light_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="obj_lvl1_light_01" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="obj_lvl1_light_01.png" width="64" height="64"/>
+</tileset>
