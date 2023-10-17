6ca1231e503ecb501c8746e686e8283305e795dc
alva531 <alvaro.jorge531@gmail.com>
2023-10-17 15:10:58 -0300

    dust anim

--- Assets/Scripts/VFX/Dust.cs
+++ Assets/Scripts/VFX/Dust.cs
@@ -7,18 +7,31 @@ public class Dust : MonoBehaviour
 {
     // Start is called before the first frame update
     SpriteRenderer _sp;
+    Transform _position;
 
     void Start()
     {
         _sp = GetComponent<SpriteRenderer>();
-              
+        _position = GetComponent<Transform>();
     }
 
     public void Initialize(Player.parama a)
     {
         transform.position = a.b;
         _sp = GetComponent<SpriteRenderer>();
-        _sp.flipX = a.a; 
+        _sp.flipX = a.a;
+        if (GameManager.INSTANCE.PLAYER.GetComponent<SpriteRenderer>().flipX)
+        {
+            _sp.flipX = false;
+            transform.position = new Vector2(transform.position.x +0.15f, transform.position.y -0.018f);
+        }
+        else
+        {
+            _sp.flipX = true;
+            transform.position = new Vector2(transform.position.x -0.15f, transform.position.y -0.018f);
+        }
+        //_sp = GetComponent<SpriteRenderer>();
+        //_sp.flipX = a.a;
         Destroy(gameObject, 0.8f);  
     }
 
