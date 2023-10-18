d14a397611853557165af00acd608434e256c8eb
alva531 <alvaro.jorge531@gmail.com>
2023-10-18 12:32:13 -0300

    hallway anim

---
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_elevator_1.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_elevator_1" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="bg_lvl1_elevator_1.png" width="128" height="64"/>
+ <tile id="2">
+  <animation>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="2" duration="700"/>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="0" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="3" duration="700"/>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="1" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="4" duration="700"/>
+   <frame tileid="6" duration="700"/>
+   <frame tileid="4" duration="700"/>
+   <frame tileid="4" duration="700"/>
+   <frame tileid="4" duration="700"/>
+   <frame tileid="4" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="5" duration="700"/>
+   <frame tileid="7" duration="700"/>
+   <frame tileid="5" duration="700"/>
+   <frame tileid="5" duration="700"/>
+   <frame tileid="5" duration="700"/>
+   <frame tileid="5" duration="700"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_hallway_05.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_hallway_05" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="bg_lvl1_hallway_05.png" width="192" height="64"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="0" duration="700"/>
+   <frame tileid="3" duration="700"/>
+   <frame tileid="0" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="1" duration="700"/>
+   <frame tileid="4" duration="700"/>
+   <frame tileid="1" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="2" duration="700"/>
+   <frame tileid="2" duration="700"/>
+   <frame tileid="5" duration="700"/>
+   <frame tileid="2" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="6" duration="700"/>
+   <frame tileid="6" duration="700"/>
+   <frame tileid="9" duration="700"/>
+   <frame tileid="6" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="7" duration="700"/>
+   <frame tileid="7" duration="700"/>
+   <frame tileid="10" duration="700"/>
+   <frame tileid="7" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="8" duration="700"/>
+   <frame tileid="8" duration="700"/>
+   <frame tileid="11" duration="700"/>
+   <frame tileid="8" duration="700"/>
+  </animation>
+ </tile>
+</tileset>
