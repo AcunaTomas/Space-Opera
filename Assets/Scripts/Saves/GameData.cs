c0ef4aa4378d5c32f47d38e57526bd330e8981db
Sergio Damián Bressán <sergiodb95@gmail.com>
2023-10-22 17:36:15 -0300

    fix_achievements

--- Assets/Scripts/Saves/GameData.cs
+++ Assets/Scripts/Saves/GameData.cs
@@ -16,7 +16,7 @@ public class GameData
     //LEVEL 1
     public bool[] CANVAS_WS_LVL1_GENERAL = {true, true, true, true, true};
     public bool[] CANVAS_WS_LVL1_ACTIVATE_EVENTS = {false, false, false, false, false};
-    public bool[] TUTO_TRIG_LVL1 = {true, false};
+    public bool[] TUTO_TRIG_LVL1 = {true, true, true, false};
     public bool[] CHECKPOINTS_LVL1 = {true, true, true, true, true, true, true, true};
     public bool[] DIALOGUES_LVL1 = {true, true, true, true, true, true, true, true, true, true, true, true, true, true, false};
     public bool[] CHANGE_MUSIC_LVL1 = {false, true};
