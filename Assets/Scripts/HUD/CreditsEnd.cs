544e4da18262e95b3012ec1ec0bfa2dc4b1f3b0d
alva531 <alvaro.jorge531@gmail.com>
2023-11-06 22:14:00 -0300

    mandarina 14? no sé

--- Assets/Scripts/HUD/CreditsEnd.cs
+++ Assets/Scripts/HUD/CreditsEnd.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndOfDemoShowcase : MonoBehaviour
+public class CreditsEnd : MonoBehaviour
 {
-
     void Start()
     {
         StartCoroutine(EndMySuffering());
+
+        AudioManager.INSTANCE.PlayMusic();
     }
 
 
     IEnumerator EndMySuffering()
     {
-        yield return new WaitForSeconds(3f);
-        ScenesManager.Instance.LoadNextScene("Credits");
+        yield return new WaitForSeconds(39f);
+        ScenesManager.Instance.LoadNextScene("SceneMainMenu");
     }
 }
