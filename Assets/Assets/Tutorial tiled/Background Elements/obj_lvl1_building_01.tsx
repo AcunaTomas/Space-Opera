0700f9edd52ab66692a0b28c64f274963e27ff9b
alva531 <alvaro.jorge531@gmail.com>
2024-02-15 22:14:59 -0300

    training 3

---
+++ Assets/Assets/Tutorial tiled/Background Elements/obj_lvl1_building_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="obj_lvl1_building_01" tilewidth="32" tileheight="32" tilecount="140" columns="14">
+ <image source="obj_lvl1_building_01.png" width="448" height="320"/>
+</tileset>
