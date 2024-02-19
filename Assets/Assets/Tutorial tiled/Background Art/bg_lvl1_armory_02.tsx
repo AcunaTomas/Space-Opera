15529fa8c17dce8a04075cd2aa9e1990583a92be
alva531 <alvaro.jorge531@gmail.com>
2024-02-19 17:23:34 -0300

    literally art updates

---
+++ Assets/Assets/Tutorial tiled/Background Art/bg_lvl1_armory_02.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="bg_lvl1_armory_02" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="bg_lvl1_armory_02.png" width="192" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="3" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="4" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="9" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="10" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="11" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
