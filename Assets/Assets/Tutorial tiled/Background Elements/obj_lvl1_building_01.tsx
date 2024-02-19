dad3b52ee3c430065f6f293a03b7203a1aa45c47
alva531 <alvaro.jorge531@gmail.com>
2024-02-19 12:06:40 -0300

    building

--- Assets/Assets/Tutorial tiled/Background Elements/obj_lvl1_building_01.tsx
+++ Assets/Assets/Tutorial tiled/Background Elements/obj_lvl1_building_01.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="obj_lvl1_building_01" tilewidth="32" tileheight="32" tilecount="140" columns="14">
  <image source="obj_lvl1_building_01.png" width="448" height="320"/>
+ <tile id="44">
+  <animation>
+   <frame tileid="44" duration="500"/>
+   <frame tileid="51" duration="500"/>
+   <frame tileid="114" duration="500"/>
+   <frame tileid="121" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="58">
+  <animation>
+   <frame tileid="58" duration="500"/>
+   <frame tileid="65" duration="500"/>
+   <frame tileid="128" duration="500"/>
+   <frame tileid="135" duration="500"/>
+  </animation>
+ </tile>
 </tileset>
